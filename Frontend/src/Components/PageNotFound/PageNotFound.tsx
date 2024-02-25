23ae359b94db6b7bd3ba4f628ac0820efabedef1
abdrhxyii <abdrhxyii@git>
2024-02-25 18:55:55 +0530

    projectdetail pages updated

--- Frontend/src/Components/PageNotFound/PageNotFound.tsx
+++ Frontend/src/Components/PageNotFound/PageNotFound.tsx
@@ -5,6 +5,7 @@ const PageNotFound = () => {
     const goToHome = () => {
         route('/')
     }
+    
   return (
     <div>
         <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
