c1bc7757804e5b892fc3ec36761888e06c29561f
abdrhxyii <abdrhxyii@git>
2024-02-28 21:23:10 +0530

    nodejs express route protection

--- Backend/Routers/Store.js
+++ Backend/Routers/Store.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const StoreController = require('../Controllers/Store');
 const AuthMiddleware = require('../Middleware/Auth');
+const JWTAuthMiddleware = require('../Middleware/Authentication')
 const route = express.Router();
 
-route.post('/createStore/:userId/store', AuthMiddleware, StoreController.CreateStore);
+route.post('/createStore/:userId/store', JWTAuthMiddleware.verifyToken, StoreController.CreateStore);
 
 module.exports = route;
